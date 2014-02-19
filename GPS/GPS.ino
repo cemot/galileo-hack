8bdca265e9d2f28f280d053ddd6e520d4c960a87
donwshin <donwshin@gmail.com>
2014-02-18 16:49:05 -0800

    GPS Code and modified data pushing to IOT

--- GPS/GPS.ino
+++ GPS/GPS.ino
@@ -1,37 +1,97 @@
 
 unsigned char buffer[64];                   // buffer array for data recieve over serial port
-int count=0;                                // counter for buffer array
+int count=0;     // counter for buffer array
+int index = 0;
 
 void setup()
 {
-    Serial1.begin(9600);                 // the SoftSerial baud rate
-    Serial.begin(115200);                     // the Serial port of Arduino baud rate.
+  Serial1.begin(9600);                 // the SoftSerial baud rate
+  Serial.begin(115200);                     // the Serial port of Arduino baud rate.
 }
 
 void loop()
 {
-    if (Serial1.available())                     // if date is comming from softwareserial port ==> data is comming from Serial1 shield
-    {
-        while(Serial1.available())               // reading data into char array
-        {
-            buffer[count++]=Serial1.read();      // writing data into array
-            if(count == 64)break;
-        }
-        Serial.write(buffer,count);                 // if no data transmission ends, write buffer to hardware serial port
-        clearBufferArray();                         // call clearBufferArray function to clear the storaged data from the array
-        count = 0;                                  // set counter of while loop to zero
-
-
-    }
-    if (Serial.available())                 // if data is available on hardwareserial port ==> data is comming from PC or notebook
-    Serial1.write(Serial.read());        // write it to the SoftSerial shield
+  while (1) {
+    int longitude = 0, latitude = 0;
+    if (getChar() != '\n') continue;
+    if (getChar() != '$') continue;
+    if (getChar() != 'G') continue;
+    if (getChar() != 'P') continue;
+    if (getChar() != 'G') continue;
+    if (getChar() != 'L') continue;
+    if (getChar() != 'L') continue;
+    if (getChar() != ',') continue;
+    
+    
+    
+    
+    
+  }
 }
-
-void clearBufferArray()                     // function to clear buffer array
-{
-    for (int i=0; i<count;i++)
-    { buffer[i]=NULL;}                      // clear all index of array with command NULL
+char getChar() {
+while (1) {
+if (Serial1.available()) {
+  return Serial1.read();
 }
+}
+}
+//void extract() {
+//  if (Serial1.available())                     // if date is comming from softwareserial port ==> data is comming from Serial1 shield
+//  {
+//    while(Serial1.available())               // reading data into char array
+//    {
+//      buffer[count++]=Serial1.read();      // writing data into array
+//      if(count == 64)break;
+//    }
+//
+//    while(True) {
+//      if (buffer[index] == '\n') {
+//        if (buffer[index+1] == '$') {
+//          if (buffer[index+2] == 'G') {
+//            if (buffer[index+3] == 'P') {
+//              if (buffer[index+4] == 'G') {
+//                if (buffer[index+5] == 'L') {
+//                  if (buffer[index+6] == 'L') {
+//                    index = index + 8;
+//                    longitude = getFloat();
+//                    while (buffer[index] != ',') {
+//                      index++;
+//                    }
+//                    
+//                    }
+//                  }
+//                }
+//              }
+//            }
+//          }
+//        }
+//
+//        index++;
+//
+//    }
+//
+//  }
+//  
+//}
+float getFloat(char* p) {
+
+  int numOfSym = 0;
+  float num = 0; 
+  for (; *p != '.'; ++p) num = 10*num + *p - '0'; 
+
+}
+//void clearBufferArray()                     // function to clear buffer array
+//{
+//  for (int i=0; i<count;i++)
+//  { 
+//    buffer[i]=NULL;
+//  }                      // clear all index of array with command NULL
+//}
+
+
+
+
+
 
 
 
--- temp_heart/temp_heart.ino
+++ temp_heart/temp_heart.ino
@@ -15,13 +15,22 @@ double change = 0.0;
 int pin = 0;
 double bodytemp;
 
+//for gps
+unsigned char buffer[64];                   // buffer array for data recieve over serial port
+int count=0; 
+int longitude = 0, latitude = 0, digit;
+
 void setup ()
 {
   pinMode (ledPin, OUTPUT);
   Serial.begin (115200);  
+  Serial1.begin(9600);                 // the SoftSerial baud rate
   iotkit.begin();
   iotkit.registerMetric("heartbeat", "float", "BPS");
   iotkit.registerMetric("bodytemp", "float", "Fahrenheit");
+  iotkit.registerMetric("lon", "int", "Degrees");
+  iotkit.registerMetric("lat", "int", "Degrees");
+  for (int j = 0; j < 400; j++) {getChar();}
 }
 
 void loop ()
@@ -30,18 +39,74 @@ void loop ()
   static double oldChange = 0;
   int rawValue = analogRead (sensorPin);
   double value = alpha * oldValue + (1 - alpha) * rawValue;
-  Serial.print (rawValue);
+     Serial.print (rawValue);
   Serial.print (", ");
   Serial.println (value);
   oldValue = value;
   iotkit.send("heartbeat", value);
-    
+
   bodytemp = Thermister(analogRead(pin));
   Serial.print(bodytemp); // display Fahrenheit
   Serial.println("f");
   iotkit.send("bodytemp", bodytemp);
-  //delay(1000);
-  delay (1000);
+ if (count == 10) {
+  while (true) {
+    
+   //if (getChar() != '\n') continue;
+  
+//    if (getChar() != '$'){ continue;}
+
+    if (getChar() != 'G') continue;
+
+    if (getChar() != 'P') continue;
+    if (getChar() != 'G') continue;
+    if (getChar() != 'L') continue;
+    if (getChar() != 'L') continue;
+    if (getChar() != ',') continue;
+    
+    for (digit = getChar(), latitude = 0; digit != '.'; digit = getChar()) {
+      latitude = 10*latitude + digit - '0';
+    }
+    for (int i = 0; i < 8; i++) getChar();
+    for (digit = getChar(), longitude = 0; digit != '.'; digit = getChar()) {
+      longitude = 10*longitude + digit - '0';
+    } 
+    iotkit.send("lon", longitude);
+  iotkit.send("lat", latitude);
+  Serial.println("data below");
+  Serial.println(longitude);
+  Serial.println(latitude);
+  longitude = 0;
+  latitude = 0;
+
+  break;
+  
+  }count = 0;}
+  count++;
+  
+  
+  
+  
+  
+  
+  
+//  //delay(1000);
+//  if (Serial1.available())                     // if date is comming from softwareserial port ==> data is comming from Serial1 shield
+//  {
+//    while(Serial1.available())               // reading data into char array
+//    {
+//      buffer[count++]=Serial1.read();      // writing data into array
+//      if(count == 64)break;
+//    }
+//    Serial.write(buffer,count);                 // if no data transmission ends, write buffer to hardware serial port
+//    clearBufferArray();                         // call clearBufferArray function to clear the storaged data from the array
+//    count = 0;                                  // set counter of while loop to zero
+//
+//
+//  }
+//  if (Serial.available())                 // if data is available on hardwareserial port ==> data is comming from PC or notebook
+//    Serial1.write(Serial.read());        // write it to the SoftSerial shield
+delay(400);
 }
 
 double Thermister (int RawADC) {
@@ -51,3 +116,19 @@ double Thermister (int RawADC) {
   Temp = Temp - 273.15; // Convert Kelvin to Celcius
   return Temp*(9/5) + 32 + 35;
 }
+void clearBufferArray()                     // function to clear buffer array
+{
+  for (int i=0; i<count;i++)
+  { 
+    buffer[i]=NULL;
+  }                      // clear all index of array with command NULL
+}
+
+char getChar() {
+
+if (Serial1.available()) {
+  return Serial1.read();
+
+}
+}
+
